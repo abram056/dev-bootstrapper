1ac8794dc008f31bc52a9dd83b1fad9cd91e118b
abram056 <shesham_abraham.mail@proton.me>
2026-04-06 13:44:55 +0100

    automated venv creation and dependency installation

--- core/generator.py
+++ core/generator.py
@@ -2,6 +2,8 @@ from pathlib import Path
 from engine.file_manager import write_file
 from engine.renderer import render
 from templates.loader import load_template
+import subprocess
+import os
 
 
 def generate(context: dict[str, str]):
@@ -18,3 +20,21 @@ def generate(context: dict[str, str]):
 
         rendered_content = render(content, context)
         write_file(output_dir, relative_path, rendered_content)
+
+    create_virtual_env(output_dir)
+    install_dependencies(output_dir)
+
+
+def create_virtual_env(project_path: Path):
+    subprocess.run(["python", "-m", "venv", ".venv"], cwd=project_path)
+
+
+def install_dependencies(project_path: Path):
+    pip_path = ""
+    if os.name == "nt":
+        pip_path = ".venv/Scripts/pip.exe"
+    else:
+        pip_path = ".venv/bin/pip"
+
+    subprocess.run([str(pip_path), "install", "-r",
+                   "requirements.txt"], cwd=project_path)
